90e409598c9993ad30dbbbcb41fe008a4de9f751
AkramDadKhan1 <akram.khan@fyntune.com>
2024-07-13 17:06:53 +0530

    UI DEMO DONE

--- ecommerce-frontend/src/pages/cart.tsx
+++ ecommerce-frontend/src/pages/cart.tsx
@@ -1,9 +1,67 @@
-import React from 'react'
-
+import React from "react";
+import { useEffect, useState } from "react";
+import { VscError } from "react-icons/vsc";
+import CartItemCard from "../components/cart-item";
+import { Link } from "react-router-dom";
+const cartItems = [
+  {
+    productId: "asdddd",
+    name: "Macbook",
+    price: 4535,
+    stock: 455,
+    photo: "https://m.media-amazon.com/images/I/71TPda7cwUL._SX679_.jpg",
+    quantity: 4,
+  },
+];
+const subTotal = 4000;
+const tax = Math.round(subTotal * 0.18);
+const shippingCharges = 200;
+const discount = 400;
+const total = subTotal + tax + shippingCharges;
 const Cart = () => {
+  const [couponCode, setCouponCode] = useState<string>("");
+  const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
   return (
-    <div>Cart</div>
-  )
-}
+    <div className="cart">
+      <main>
+        {cartItems.length > 0 ? (
+          cartItems.map((i, idx) => <CartItemCard key={idx} cartItem={i} />)
+        ) : (
+          <h1>No Items Added</h1>
+        )}
+      </main>
+      <aside>
+        <p>Subtotal: ₹{subTotal}</p>
+        <p>Shipping Charges: ₹{shippingCharges}</p>
+        <p>Tax: ₹{tax}</p>
+        <p>
+          Discount: <em className="red"> - ₹{discount}</em>
+        </p>
+        <p>
+          <b>Total: ₹{total}</b>
+        </p>
 
-export default Cart
+        <input
+          type="text"
+          placeholder="Coupon Code"
+          value={couponCode}
+          onChange={(e) => setCouponCode(e.target.value)}
+        />
+
+        {couponCode &&
+          (isValidCouponCode ? (
+            <span className="green">
+              ₹{discount} off using the <code>{couponCode}</code>
+            </span>
+          ) : (
+            <span className="red">
+              Invalid Coupon <VscError />
+            </span>
+          ))}
+        {cartItems.length > 0 && <Link to="/shipping">Checkout</Link>}
+      </aside>
+    </div>
+  );
+};
+
+export default Cart;
--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,23 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 
 const Home = () => {
+  const addToCartHandler = () => {};
   return (
     <div className="home">
-    <section></section>
+      <section></section>
 
-    <h1>
-      Latest Products
-      <Link to="/search" className="findmore">
-        More
-      </Link>
-    </h1>
+      <h1>
+        Latest Products
+        <Link to="/search" className="findmore">
+          More
+        </Link>
+      </h1>
 
-    <main>
+      <main>
+        <ProductCard
+          productId="asdddd"
+          name="Macbook"
+          price={4535}
+          stock={455}
+          handler={addToCartHandler}
+          photo="https://m.media-amazon.com/images/I/71TPda7cwUL._SX679_.jpg"
+        />
+      </main>
+    </div>
+  );
+};
 
-    </main>
-  </div>
-  )
-}
-
-export default Home
+export default Home;
